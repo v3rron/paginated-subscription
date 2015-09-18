8010d8e0f639d738fa7ed5c21fa48c0fd405911d
Ronen Verdi <go7hic@gmail.com>
2015-09-18 17:33:24 -0400

    git URL changed

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'verron:paginated-subscription',
   summary: "Easily paginate a subscription that takes a limit parameter and initial number of pages.",
   version: "0.3.0",
-  git: "https://github.com/percolatestudio/paginated-subscription.git"
+  git: "https://github.com/v3rron/paginated-subscription.git"
 });
 
 Package.onUse(function (api, where) {
